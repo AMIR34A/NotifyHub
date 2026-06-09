ee4791ad08538a863b72a69bd0f80391e1b3fbad
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-06-09 16:20:24 +0330

    Add new functionality to the Json service.

--- src/1.Core/NotifyHub.Core.Contracts/Services/IJsonSerializerService.cs
+++ src/1.Core/NotifyHub.Core.Contracts/Services/IJsonSerializerService.cs
@@ -4,7 +4,11 @@ public interface IJsonSerializerService
 {
     string Serialize<TInput>(TInput input);
 
+    ReadOnlyMemory<byte> SerializeToUtf8Bytes<TInput>(TInput input);
+
     object? Deserialize(string input, Type type);
+
+    TOutput? Deserialize<TOutput>(ReadOnlySpan<byte> span);
 
     TOutput? Deserialize<TOutput>(string input);
 }
---
+++ src/2.Infrastructure/NotifyHub.Infrastructure.Services/Jsons/JsonSerializerService.cs
@@ -0,0 +1,17 @@
+﻿using NotifyHub.Core.Contracts.Services;
+using System.Text.Json;
+
+namespace NotifyHub.Infrastructure.Services.Json;
+
+public class JsonSerializerService : IJsonSerializerService
+{
+    public object? Deserialize(string input, Type type) => JsonSerializer.Deserialize(input, type);
+
+    public TOutput? Deserialize<TOutput>(string input) => JsonSerializer.Deserialize<TOutput>(input);
+
+    public TOutput? Deserialize<TOutput>(ReadOnlySpan<byte> span) => JsonSerializer.Deserialize<TOutput>(span);
+
+    public string Serialize<TInput>(TInput input) => JsonSerializer.Serialize(input);
+
+    public ReadOnlyMemory<byte> SerializeToUtf8Bytes<TInput>(TInput input) => JsonSerializer.SerializeToUtf8Bytes<TInput>(input);
+}
