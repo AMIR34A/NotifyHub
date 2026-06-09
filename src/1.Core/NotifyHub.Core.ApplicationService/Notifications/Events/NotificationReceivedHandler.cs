2fd33d6301ce1b618212cd0c389d86c02c7d8ab7
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-06-09 16:22:58 +0330

    Add NotificationReceived event and its handler.

---
+++ src/1.Core/NotifyHub.Core.ApplicationService/Notifications/Events/NotificationReceivedHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NotifyHub.Core.ApplicationService.Notifications.Commands.Create;
+using NotifyHub.Core.Contracts.Services;
+using NotifyHub.Core.Domain.Notifications;
+using NotifyHub.Infrastructure.Services.MessageBuses.RabbitMq;
+
+namespace NotifyHub.Core.ApplicationService.Notifications.Events
+{
+    internal class NotificationReceivedHandler : RabbitMqConsumer<NotificationReceived>
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationReceivedHandler(IMediator mediator,
+            RabbitMqConnectionManager connectionManager,
+            IJsonSerializerService jsonSerializerService,
+            ILogger<NotificationReceivedHandler> logger) : base(connectionManager, jsonSerializerService, logger)
+        {
+            _mediator = mediator;
+        }
+
+        protected async override Task HandleAsync(NotificationReceived message, CancellationToken cancellationToken)
+        {
+            var command = new CreateNotificationCommand(message.Channel,
+                message.Message,
+                message.Parameters,
+                message.Data,
+                message.RequestedBy);
+
+            await _mediator.Send(command, cancellationToken);
+        }
+    }
+}
---
+++ src/1.Core/NotifyHub.Core.Domain/Notifications/NotificationReceived.cs
@@ -0,0 +1,9 @@
+﻿using NotifyHub.Core.BuildingBlocks.Events;
+
+namespace NotifyHub.Core.Domain.Notifications;
+
+public sealed record NotificationReceived(Channel Channel,
+    Message Message,
+    ICollection<Parameter> Parameters,
+    string Data,
+    string RequestedBy) : IDomainEvent;
