2b0222c457e56e3ef4d41b84a34a6e667e10a324
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-07-30 18:08:58 +0330

    Add GetUnprocessedNotifications and GetNotificationsWithRetryStatus to the NotifiactionRepository.

--- src/1.Core/NotifyHub.Core.Contracts/Data/Notifications/INotificationRepository.cs
+++ src/1.Core/NotifyHub.Core.Contracts/Data/Notifications/INotificationRepository.cs
@@ -5,4 +5,7 @@ namespace NotifyHub.Core.Contracts.Data.Notifications;
 
 public interface INotificationRepository : IBaseRepository<Notification, Guid>, IUnitOfWork
 {
+    Task<IEnumerable<Notification>> GetUnprocessedNotifications(int size, CancellationToken cancellationToken);
+
+    Task<IEnumerable<Notification>> GetNotificationsWithRetryStatus(int size, CancellationToken cancellationToken);
 }
--- src/2.Infrastructure/Data/NotifyHub.Infrastructure.Data.SqlServer/Notifications/NotificationRepository.cs
+++ src/2.Infrastructure/Data/NotifyHub.Infrastructure.Data.SqlServer/Notifications/NotificationRepository.cs
@@ -1,4 +1,5 @@
-﻿using NotifyHub.Core.Contracts.Data.Notifications;
+﻿using Microsoft.EntityFrameworkCore;
+using NotifyHub.Core.Contracts.Data.Notifications;
 using NotifyHub.Core.Domain.Notifications;
 using NotifyHub.Infrastructure.Data.SqlServer.Base;
 
@@ -9,4 +10,18 @@ public class NotificationRepository : BaseRepository<Notification, NotifyHubDbCo
     public NotificationRepository(NotifyHubDbContext dbContext) : base(dbContext)
     {
     }
+
+    public async Task<IEnumerable<Notification>> GetUnprocessedNotifications(int size, CancellationToken cancellationToken) => await _dbContext.Notifications
+        .Where(n => n.Status == Status.InQueue)
+        .Take(size)
+        .Include(n => n.Message)
+        .Include(n => n.Parameters)
+        .ToListAsync(cancellationToken);
+
+    public async Task<IEnumerable<Notification>> GetNotificationsWithRetryStatus(int size, CancellationToken cancellationToken) => await _dbContext.Notifications
+        .Where(n => n.Status == Status.Retry && n.RetryCount < Notification.MaxRetryCount)
+        .Take(size)
+        .Include(n => n.Message)
+        .Include(n => n.Parameters)
+        .ToListAsync(cancellationToken);
 }
