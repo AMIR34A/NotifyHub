5f4e3bfceee5d832fd2e98462c2cfda5dc1dc538
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-08-06 08:19:33 +0330

    Add new validation for payload data.

--- src/2.Infrastructure/NotifyHub.Infrastructure.Services/SMSs/SmsService.cs
+++ src/2.Infrastructure/NotifyHub.Infrastructure.Services/SMSs/SmsService.cs
@@ -17,12 +17,15 @@ public class SmsService(IEnumerable<ISmsProvider> smsProviders,
     {
         Guard.ThrowExceptionIf.Empty(payload, new ServiceException(Error.Failure()));
         Guard.ThrowExceptionIf.Empty(message, new ServiceException(Error.Failure()));
+
         SmsPayload? smsPayload = jsonSerializer.Deserialize<SmsPayload>(payload);
+
         Guard.ThrowExceptionIf.Null(smsPayload, new ServiceException(Error.Failure()));
+        Guard.ThrowExceptionIf.Null(smsPayload!.Receiver, new ServiceException(Error.Failure()));
 
         foreach (var smsProvider in smsProviders)
         {
-            var result = await smsProvider.SendAsync(smsPayload!.Receiver, message, cancellationToken);
+            var result = await smsProvider.SendAsync(smsPayload.Receiver, message, cancellationToken);
 
             if (result.Succeed)
                 return true;
